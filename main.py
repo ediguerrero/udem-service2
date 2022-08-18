c8fa97cf4c8c8ec545afb551826c7d3637b91208
ediguerrero <edixon_guerrero96@hotmail.com>
2022-08-17 21:17:39 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import Union
+
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+@app.get("/authUsers/{internalId}")
+def read_item(internalId: str):
+    url = 'https://62fc4666abd610251c17fdae.mockapi.io/api/authUsers?internalId=' + internalId
+    response = requests.get(url, {}, timeout=5)
+    return {"items": response.json()}
