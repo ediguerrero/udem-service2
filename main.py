85e6b68ac47f8fbac63b32a41a546834e07083d7
ediguerrero <edixon_guerrero96@hotmail.com>
2022-08-17 21:21:06 -0500

    change something

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 
 @app.get("/authUsers/{internalId}")
 def read_item(internalId: str):
-    url = 'https://62fc4666abd610251c17fdae.mockapi.io/api/authUsers?internalId=' + internalId
+    url = 'https://62fc4666abd610251c17fdae.mockapi.io/api/authUsers?internalId='+internalId
     response = requests.get(url, {}, timeout=5)
     return {"items": response.json()}
